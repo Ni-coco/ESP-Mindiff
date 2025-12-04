0922ed77e8ac32bdc419b7036c142690c1e76107
Ni-coco <nicolas.garde@outlook.com>
2025-12-04 15:21:25 +0100

    chore: update dependencies and refactor main app structure

---
+++ app/lib/pages/programme_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ProgrammePage extends StatelessWidget {
+  const ProgrammePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Programme'),
+    );
+  }
+}
